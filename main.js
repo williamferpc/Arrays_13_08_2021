6a1e8cf2ab3e37b342bd551d81eaa8f73da3ed60
williamferpc <willi_9212@hotmail.com>
2021-08-20 20:09:45 -0500

    arrays 13-08-2021

---
+++ main.js
@@ -0,0 +1,14 @@
+let array = [];
+// array = ['d', 'a', 'm', 'a'];
+// array.copyWithin(0, 2);
+// console.log(array);
+
+
+// array = ['r', 'a', 'm', 'o'];
+// array.copyWithin(-1, 1);
+// console.log(array);
+
+array = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+array.copyWithin(0, 2);
+// array.copyWithin(1, 2);
+console.log(array);
